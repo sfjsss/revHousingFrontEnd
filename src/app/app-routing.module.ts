085bc2f05fd114060b96c8623a888a21fa4ae749
Tianyu Li <alandron06281990@gmail.com>
2020-03-10 20:10:55 -0500

    setup form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SearchComponent } from './components/dashboard/search/search.component';
+import { CreatePostComponent } from './components/dashboard/create-post/create-post.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent, children: [
-    { path: '', component: SearchComponent }
+    { path: '', component: SearchComponent },
+    { path: 'create-post', component: CreatePostComponent }
   ]}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,12 +12,14 @@ import { MaterialModule } from './material.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SearchComponent } from './components/dashboard/search/search.component';
 import { AgmCoreModule } from '@agm/core';
+import { CreatePostComponent } from './components/dashboard/create-post/create-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    SearchComponent
+    SearchComponent,
+    CreatePostComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +29,7 @@ import { AgmCoreModule } from '@agm/core';
     FlexLayoutModule,
     HttpClientModule,
     FormsModule,
+    RouterModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyC31fPvxdguqVUnokZKGqe_GSwhnIjv-go'
     })
