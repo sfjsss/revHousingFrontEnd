6ba8d62ba23f64fecd014ad6cddb6f6a622361d8
Vytautas Klimavicius <vyklimavicius@gmail.com>
2020-03-12 15:02:08 -0500

    Profile template done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,19 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
 import { GoodbyeComponent } from './components/goodbye/goodbye.component';
 import { MyPostsComponent } from './components/dashboard/my-posts/my-posts.component';
 import { CreatePostComponent } from './components/dashboard/create-post/create-post.component';
+import { ProfileComponent } from './components/dashboard/profile/profile.component';
 
 const routes: Routes = [
-  { path: 'welcome', component: WelcomeComponent },
-  { path: 'goodbye', component: GoodbyeComponent },
+  { path: "welcome", component: WelcomeComponent },
+  { path: "goodbye", component: GoodbyeComponent },
   {
-    path: '', component: DashboardComponent, children: [
-      { path: '', component: SearchComponent },
-      { path: 'create-post', component: CreatePostComponent },
-      { path: 'my-posts', component: MyPostsComponent }
+    path: "",
+    component: DashboardComponent,
+    children: [
+      { path: "", component: SearchComponent },
+      { path: "create-post", component: CreatePostComponent },
+      { path: "my-posts", component: MyPostsComponent },
+      { path: "profile", component: ProfileComponent }
     ]
   }
 ];
